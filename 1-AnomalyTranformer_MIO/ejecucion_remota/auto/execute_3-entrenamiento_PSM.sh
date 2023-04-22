fc5d43d07d8975bc07a8594c4e019c7d8d30eebc
ArturoSirvent <arturosirvent@outlook.com>
2023-04-22 11:21:00 +0200

    ejecuciones smap t psm

---
+++ 1-AnomalyTranformer_MIO/ejecucion_remota/auto/execute_3-entrenamiento_PSM.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+#SBATCH --job-name=my_job_arturo_try
+#SBATCH --time=11:59:00
+#SBATCH --mem=30GB
+#SBATCH --partition guest
+#SBATCH --output="/home/asirvent/extra_repos/TFM/1-AnomalyTranformer_MIO/ejecucion_remota/logs/execute_3-entrenamiento_PSM.out"
+
+source /home/asirvent/pytorch_env/bin/activate 
+cd /home/asirvent/extra_repos/Anomaly-Transformer
+bash /home/asirvent/extra_repos/Anomaly-Transformer/scripts/PSM.sh
