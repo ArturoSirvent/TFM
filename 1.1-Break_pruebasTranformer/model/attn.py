78f3ee4548f843dcd3eb803c82b975b908525e1a
ArturoSirvent <arturosirvent@outlook.com>
2023-04-28 21:50:47 -0400

    cosillas mas datos sinteticos ahora context

--- 1.1-Break_pruebasTranformer/model/attn.py
+++ 1.1-Break_pruebasTranformer/model/attn.py
@@ -48,7 +48,7 @@ class AnomalyAttention(nn.Module):
         #esto se añade porque si, relamente no lo pone el paper  
         sigma = torch.sigmoid(sigma * 5) + 1e-5
         sigma = torch.pow(3, sigma) - 1
-        #sigma=torch.ones_like(sigma)
+    
         sigma = sigma.unsqueeze(-1).repeat(1, 1, 1, window_size)  # B H L L
         prior = self.distances.unsqueeze(0).unsqueeze(0).repeat(sigma.shape[0], sigma.shape[1], 1, 1).cuda()
         prior = 1.0 / (math.sqrt(2 * math.pi) * sigma) * torch.exp(-prior ** 2 / 2 / (sigma ** 2))
